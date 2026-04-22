01de7969f7d6a9c6d3fea98896341b2efe54feab
Noah Beaudoin <noahgames@taconictech.com>
2026-04-22 14:47:55 -0400

    Added printPreOrder() and printPostOrder to the print() in BST.h

--- BST.h
+++ BST.h
@@ -89,8 +89,12 @@ public:
 
   void print() const { // Print the contents of the BST
     if (root == NULL) cout << "The BST is empty.\n";
-    else printhelp(root, 0);
-  }
+    else 
+    {
+        printhelp(root, 0);
+        printPreOrder();
+        printPostOrder();
+    }
   
 };
 
